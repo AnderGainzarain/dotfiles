9459719de6cc5dcdc1f2072fc8a30353b752bcd0
AnderGainzarain <71083491+AnderGainzarain@users.noreply.github.com>
2022-09-05 15:39:10 +0200

    Delete Dependencies/rofi directory

--- Dependencies/rofi/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-chosen=$(printf "  Power Off\n  Restart" | rofi -dmenu -i -theme-str '@import "material.rasi"')
-
-case "$chosen" in
-	"  Power Off") poweroff ;;
-	"  Restart") reboot ;;
-	*) exit 1 ;;
-esac
