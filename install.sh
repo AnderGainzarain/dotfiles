883ad90c58291f8b71ec251f16ada1ce5725dce2
AnderGainzarain <71083491+AnderGainzarain@users.noreply.github.com>
2022-09-05 18:09:34 +0200

    Added a script to move each file to their location

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# move the wallpapers to the pictures folder
+mv wallpapers ~/Pictures/Wallpapers
+
+# move the configs to their respective place
+mv config/zshrc ~/.zshrc
+mv config/* ~/.config
+
+# move the dependencies to their respective places
+mv Dependencies/Scripts/* ~/.Scripts
+
+# move the rofi theme
+mv Dependencies/material.rasi /usr/share/rofi/themes
