27b42ccb6d1fb8b66de0199c1510db2a5c5ca75e
againzarain <againzarain@katoid.com>
2024-11-18 12:47:04 +0100

    Deleted the old files that did not match the gnu stow structure

--- .config/nvim/lua/ander/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_streing({ search = vim.fn.input("Grep >")});
-end)
--- .config/waybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# Quit running waybar instances
-killall waybar
-
-# Load the waybar configuration
-waybar
