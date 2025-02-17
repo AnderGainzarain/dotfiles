515b99ca1ecdd040f05a8b9838803b55b4057430
againzarain <againzarain@katoid.com>
2025-02-17 08:03:31 +0100

    Disabled the treesitter plugin for nvim

--- nvim/.config/nvim/lua/ander/plugins.lua
+++ nvim/.config/nvim/lua/ander/plugins.lua
@@ -80,14 +80,14 @@ return packer.startup(function(use)
     "neovim/nvim-lspconfig",
   }
   -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-  }
-  use "p00f/nvim-ts-rainbow" -- extra highlight for Treesitter
+  -- use {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   run = function()
+  --     local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+  --     ts_update()
+  --   end,
+  -- }
+  -- use "p00f/nvim-ts-rainbow" -- extra highlight for Treesitter
   
   -- Indent Rainbow
   use "lukas-reineke/indent-blankline.nvim"
